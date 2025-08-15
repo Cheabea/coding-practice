400717e764147c95f89b3bafa143dab17bd8cd50
Ayush <ayushamanpany@gmail.com>
2025-08-15 12:28:14 +0530

    Add matrix input and print program

---
+++ matrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Number of rows: ";
+    cin >> rows;
+    cout << "Number of columns: ";
+    cin >> cols;
+
+    int arr[rows][cols];
+    cout << "Enter the elements of the matrix:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "The matrix is:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
